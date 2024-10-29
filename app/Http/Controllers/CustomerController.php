84e55fcfcf6cb01e7a9b28f7ab1e855f3fbcf435
terry-man-com <terry.man.com@icloud.com>
2024-10-29 13:26:44 +0900

    編集機能完成

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Customer;
+use Illuminate\Http\Request;
+use PHPUnit\Framework\MockObject\Stub\ReturnReference;
+
+class CustomerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $customers = Customer::all();
+        return view('customers.index', compact('customers'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('customers.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $customer = new Customer();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Customer  $customer
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Customer $customer)
+    {
+        return view('customers.show', compact('customer'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Customer  $customer
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Customer $customer)
+    {
+        return view('customers.edit', compact('customer'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Customer  $customer
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Customer $customer)
+    {
+        $customer->name    = $request->name;
+        $customer->mail    = $request->mail;
+        $customer->zipcode = $request->zipcode;
+        $customer->address = $request->address;
+        $customer->tel     = $request->tel;
+        
+        $customer->save();
+        return redirect()->route('customers.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Customer  $customer
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Customer $customer)
+    {
+        $customer->delete();
+        return redirect()->route('customers.index');
+    }
+}
---
+++ resources/views/customers/create.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+
+@section('title', '郵便番号検索')
+
+@section('content')
+    <h1>郵便番号検索画面</h1>
+@endsection
