07f4e7386318243acbabc20546523544d5b33b2f
terry-man-com <terry.man.com@icloud.com>
2024-10-30 07:51:32 +0900

    データ検証機能追加

--- resources/views/customers/search.blade.php
+++ resources/views/customers/search.blade.php
@@ -4,6 +4,18 @@
 
 @section('content')
     <h1>新規登録画面</h1>
+    @if ($errors->any())
+        <div class="error">
+            <p>
+                <b>{{ count($errors) }}件のエラーがあります。</b>
+            </p>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
     <form action="{{ route('customers.store') }}" method="post">
         @csrf
         <div>
@@ -11,16 +23,16 @@
             <input type="text" id="name" name="name" value="{{ old('name') }}">
         </div>
         <div>
-            <label for="mail">メールアドレス</label>
-            <input type="text" id="mail" name="mail" value="{{ old('mail') }}">
+            <label for="email">メールアドレス</label>
+            <input type="email" id="email" name="mail" value="{{ old('mail') }}">
         </div>
         <div>
             <label for="zipcode">郵便番号</label>
-            <input type="text" id="zipcode" name="zipcode" value={{ $results->results[0]->zipcode }}>
+            <input type="text" id="zipcode" name="zipcode" value={{ old('zipcode', $results->results[0]->zipcode) }}>
         </div>
         <div>
             <label for="address">住所</label>
-            <textarea name="address" id="address" cols="30" rows="5">{{ $address }}</textarea>
+            <textarea name="address" id="address" cols="30" rows="5">{{ old('address', $address) }}</textarea>
         </div>
         <div>
             <label for="tel">電話番号</label>
